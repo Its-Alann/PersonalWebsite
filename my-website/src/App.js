3dc02803675235b28963d23e0815539690956218
Its-Alann <76508163+Its-Alann@users.noreply.github.com>
2023-01-12 18:45:32 -0500

    Started web app

---
+++ my-website/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { NavbarCustom } from './components/NavbarCustom'
+
+function App() {
+  return (
+    <div>
+      <NavbarCustom/>
+    </div>
+  );
+}
+
+export default App;
