4c9928a3214237445fbabcb260466dbd404a85c4
Its-Alann <76508163+Its-Alann@users.noreply.github.com>
2023-05-02 22:34:31 -0400

    Added about section and social links

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,11 +1,15 @@
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
 
 function App() {
   return (
-    <div> 
-      <Navbar/>
-      <Home/>
+    <div>
+      <Navbar />
+      <Home />
+      <SocialLinks />
+      <About />
     </div>
   );
 }
---
+++ my-website/src/components/Home.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { MdKeyboardArrowRight } from "react-icons/md";
+
+const Home = () => {
+  return (
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b
+     from-black via-black to-gray-900 text-white"
+    >
+      <div
+        className="max-w-screen-lg mx-auto flex flex-col items-center
+         justify-center h-full px-4 md:flex-row"
+      >
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl">Hi! I'm Alan</h2>
+          <p className="text-gray-500 py-4 max-w-md items-center justify-center flex">
+            I am a third year Software Engineering student at Concordia
+            University in Montreal
+          </p>
+
+          <div>
+            <button
+              className="group text-white w-fit px-6 py-3
+                    my-2 flex items-center rounded-md bg-gradient-to-r
+                    from-cyan-500 to-blue-500 cursor-pointer"
+            >
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdKeyboardArrowRight size={15} className="ml-1" />
+              </span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
