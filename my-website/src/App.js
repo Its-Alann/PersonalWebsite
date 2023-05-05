c421de16a38f1da0ee6d019e7289ebc0d6c33205
Its-Alann <76508163+Its-Alann@users.noreply.github.com>
2023-05-05 02:50:44 -0400

    Added projects and experience section

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -2,14 +2,18 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import SocialLinks from "./components/SocialLinks";
 import About from "./components/About";
+import Projects from "./components/Projects";
+import Experience from "./components/Experience";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
-      <SocialLinks />
       <About />
+      <Projects />
+      <Experience />
+      <SocialLinks />
     </div>
   );
 }
