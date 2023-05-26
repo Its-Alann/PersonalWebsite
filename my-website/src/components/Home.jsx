0423911ad65676f32224312fedc97f6cab44d0fd
Its-Alann <76508163+Its-Alann@users.noreply.github.com>
2023-05-26 02:54:10 -0400

    Added links to mobile and fixed text size

--- my-website/src/components/Home.jsx
+++ my-website/src/components/Home.jsx
@@ -2,8 +2,57 @@ import React from "react";
 import { MdKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
 import MyPic from "../assets/images/picture.jpg";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+// import { BsFillPersonLinesFill } from "react-icons/bs";
+import { FiFileText } from "react-icons/fi";
 
 const Home = () => {
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          <FaLinkedin size={30} />
+        </>
+      ),
+      href: "https://www.linkedin.com/in/alan-vadlakunta",
+      style: "rounded-tr-md",
+    },
+
+    {
+      id: 2,
+      child: (
+        <>
+          <FaGithub size={30} />
+        </>
+      ),
+      href: "https://github.com/Its-Alann",
+    },
+
+    {
+      id: 3,
+      child: (
+        <>
+          <HiOutlineMail size={30} />
+        </>
+      ),
+      href: "mailto:alanvadlakunta@gmail.com",
+    },
+
+    {
+      id: 4,
+      child: (
+        <>
+          <FiFileText size={30} />
+        </>
+      ),
+      href: "/Alan_Vadlakunta_CV_2023_v3.pdf",
+      style: "rounded-br-md",
+      download: true,
+    },
+  ];
+
   return (
     <div
       name="home"
@@ -15,7 +64,7 @@ const Home = () => {
          justify-center h-full px-4 md:flex-row"
       >
         <div className="flex flex-col justify-center h-80">
-          <h2 className="text-4xl sm:text-7xl">Hi! I'm Alan Vadlakunta</h2>
+          <h2 className="text-4xl sm:text-6xl">Hi! I'm Alan Vadlakunta</h2>
           <p className="text-gray-500 py-4 max-w-md">
             I'm a final year Software Engineering student at Concordia
             University in Montreal.
@@ -37,13 +86,37 @@ const Home = () => {
               </span>
             </Link>
           </div>
+
+          <div>
+            <ul>
+              {links.map(({ id, child, href, style, download }) => (
+                <li
+                  key={id}
+                  className={
+                    "xl:hidden inline-flex items-center bg-gradient-to-r my-2 mr-2 px-3 py-3 rounded-md from-cyan-500 to-blue-500 cursor-pointer " +
+                    style
+                  }
+                >
+                  <a
+                    href={href}
+                    // className="flex justify-between items-center w-full text-white"
+                    download={download}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {child}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
 
         <div className="w-2/3 md:w-2/3">
           <img
             src={MyPic}
             alt="My Profile Pic"
-            className="rounded-2xl mx-auto"
+            className="rounded-2xl mx-auto mt-8 md:mt-0"
           />
         </div>
       </div>
